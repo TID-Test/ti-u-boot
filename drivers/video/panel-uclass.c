c3233b137b3be51c3fc3d79f43328d83b6b12823
TID-Test <test@technical-id.com>
2024-10-21 08:24:28 +0200

    Revert "debug 3"
    This reverts commit be710ad18f9a1b8366520472530aa73b487e1d9e.

--- drivers/video/panel-uclass.c
+++ drivers/video/panel-uclass.c
@@ -19,8 +19,6 @@ int panel_enable_backlight(struct udevice *dev)
 	if (!ops->enable_backlight)
 		return -ENOSYS;
 
-	printf("panel-uclass: ops->enable_backlight: %d\n", ops->enable_backlight(dev));
-
 	return ops->enable_backlight(dev);
 }
 
@@ -41,12 +39,11 @@ int panel_set_backlight(struct udevice *dev, int percent)
 	if (!ops->set_backlight)
 		return -ENOSYS;
 
-	printf("panel-uclass: ops->set_backlight(dev, percent): %d\n", ops->set_backlight(dev, percent));
-
 	return ops->set_backlight(dev, percent);
 }
 
-int panel_get_display_timing(struct udevice *dev, struct display_timing *timings)
+int panel_get_display_timing(struct udevice *dev,
+							 struct display_timing *timings)
 {
 	struct panel_ops *ops = panel_get_ops(dev);
 
@@ -54,8 +51,6 @@ int panel_get_display_timing(struct udevice *dev, struct display_timing *timings
 
 	if (!ops->get_display_timing)
 		return -ENOSYS;
-
-	printf("panel-uclass: ops->get_display_timing(dev, timings): %d\n", ops->get_display_timing(dev, timings));
 
 	return ops->get_display_timing(dev, timings);
 }
